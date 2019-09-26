7176e05fd48eeb116ce9c2f7f07a4468b9b4329d
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-26 16:00:39 -0300

    Aprendendo atributos

---
+++ Atributos/Models/AlunoModel.cs
@@ -0,0 +1,14 @@
+namespace Atributos.Models
+{
+    public class AlunoModel
+    {
+        // get e set configura se pode alterar e consultar pelo nosso Program
+        // public int MyProperty { get; set; } => atributo  => Objeto   
+        public string Nome {get; set;}
+        public string Curso { get; set; }
+        public int Idade { get; set; }
+        public double Rg { get; set; }
+
+
+    }
+}
---
+++ Atributos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Atributos.Models;
+namespace Atributos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlunoModel aluno1 = new AlunoModel();
+            Console.Clear();
+            Console.WriteLine("Digite o nome do aluno: ");
+            aluno1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite o Nome do curso: ");
+            aluno1.Curso = Console.ReadLine();
+            Console.WriteLine("Digite a idade: ");
+            aluno1.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o rg do aluno: ");
+            aluno1.Rg = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine($@"
+
+            Nome => {aluno1.Nome}
+            Curso => {aluno1.Curso}
+            Idade => {aluno1.Idade}
+            RG => {aluno1.Rg}
+
+            ");
+        }
+    }
+}
