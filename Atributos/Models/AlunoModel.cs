4e9f6af2046e6c5212d44970b95f6b9fbe4a78a3
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-27 16:03:54 -0300

    Seprando e colocando tudo no models

--- Atributos/Models/AlunoModel.cs
+++ Atributos/Models/AlunoModel.cs
@@ -1,14 +1,40 @@
+using System;
+
 namespace Atributos.Models
 {
     public class AlunoModel
     {
         // get e set configura se pode alterar e consultar pelo nosso Program
         // public int MyProperty { get; set; } => atributo  => Objeto   
-        public string Nome {get; set;}
+        public string Nome { get; set; }
         public string Curso { get; set; }
         public int Idade { get; set; }
         public double Rg { get; set; }
 
+        public void cadastrarAluno()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o nome do aluno: ");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Digite o Nome do curso: ");
+            Curso = Console.ReadLine();
+            Console.WriteLine("Digite a idade: ");
+            Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o rg do aluno: ");
+            Rg = double.Parse(Console.ReadLine());
+        }
+
+        public void listarAluno()
+        {
+            Console.WriteLine($@"
+
+            Nome => {Nome}
+            Curso => {Curso}
+            Idade => {Idade}
+            RG => {Rg}
+
+            ");
+        }
 
     }
 }
--- Atributos/Program.cs
+++ Atributos/Program.cs
@@ -7,24 +7,9 @@ namespace Atributos
         static void Main(string[] args)
         {
             AlunoModel aluno1 = new AlunoModel();
-            Console.Clear();
-            Console.WriteLine("Digite o nome do aluno: ");
-            aluno1.Nome = Console.ReadLine();
-            Console.WriteLine("Digite o Nome do curso: ");
-            aluno1.Curso = Console.ReadLine();
-            Console.WriteLine("Digite a idade: ");
-            aluno1.Idade = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o rg do aluno: ");
-            aluno1.Rg = double.Parse(Console.ReadLine());
-            Console.Clear();
-            Console.WriteLine($@"
-
-            Nome => {aluno1.Nome}
-            Curso => {aluno1.Curso}
-            Idade => {aluno1.Idade}
-            RG => {aluno1.Rg}
-
-            ");
+            aluno1.cadastrarAluno();
+            aluno1.listarAluno();
+           
         }
     }
 }
