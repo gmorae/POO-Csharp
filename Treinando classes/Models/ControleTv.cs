650de3cea7b6c1ff00330b28bc01e0814b9e92ac
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-27 14:02:38 -0300

    Mostrando o que tinha armazenado

---
+++ Treinando classes/Models/ControleTv.cs
@@ -0,0 +1,14 @@
+namespace Treinando_classes.Models
+{
+    public class ControleTv
+    {
+        public bool botao { get; set;}
+        
+        public string color {get; set;}
+
+        public bool sensor {get; set;}
+
+        public double tamanho {get; set;} 
+
+    }
+}
--- Treinando classes/Program.cs
+++ Treinando classes/Program.cs
@@ -7,26 +7,35 @@ namespace Treinando_classes
         static void Main(string[] args)
         {
             ControleTv ctrl = new ControleTv();
-            
+
             Console.WriteLine("O controle tem botão ?");
-           string resposta = Console.ReadLine();
+            string resposta = Console.ReadLine();
             ctrl.botao = verificaBool(resposta);
             Console.WriteLine("Digite a cor do botão: ");
             ctrl.color = Console.ReadLine();
             Console.WriteLine("O controle tem sensor ?");
-            ctrl.sensor = bool.Parse(Console.ReadLine());
+            resposta = Console.ReadLine();
+            ctrl.sensor = verificaBool(resposta);
+
             Console.WriteLine("Digite o tamanho: ");
             ctrl.tamanho = double.Parse(Console.ReadLine());
 
             Console.WriteLine($@"
-            
+            Botão => {ctrl.botao}
+            Cor => {ctrl.color}
+            Sensor => {ctrl.sensor}
+            Tamanho => {ctrl.tamanho} cm
             ");
         }
-        public static bool verificaBool(string resposta){
+        public static bool verificaBool(string resposta)
+        {
             resposta = resposta.ToUpper();
-            if(resposta == "SIM"){
+            if (resposta == "SIM")
+            {
                 return true;
-            }else{
+            }
+            else
+            {
                 return false;
             }
         }
