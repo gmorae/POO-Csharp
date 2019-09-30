cabf4fcfecac8e12ff39e3a1b23354609d87f9be
Gabriel Moraes <gmmartins06@gmail.com>
2019-09-30 14:26:26 -0300

    Aprendendo a mexer com herança

--- ExemploDeHeranca/Program.cs
+++ ExemploDeHeranca/Program.cs
@@ -7,17 +7,65 @@ namespace ExemploDeHeranca
     {
         static void Main(string[] args)
         {
-            FuncionarioModel func = new FuncionarioModel();
             int opcao;
+            FuncionarioModel func = new FuncionarioModel();
             do
             {
+                Console.Clear();
                 Console.WriteLine("Digite uma opçção");
                 Console.WriteLine("1 - Cadastrar Funcionario");
                 Console.WriteLine("2 - Execultar Ações");
                 Console.WriteLine("0 - Sair");
                 opcao = int.Parse(Console.ReadLine());
 
-                
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Cadstro de usuário");
+                        func.CadastrarFuncionario();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        int acao = 0;
+                        do
+                        {
+                            Console.WriteLine($"Selecione uma opção para {func.Nome}");
+                            Console.WriteLine("1 - Andar");
+                            Console.WriteLine("2 - Comer");
+                            Console.WriteLine("3 - Trabalhar");
+                            Console.WriteLine("0 - Sair");
+                            acao = int.Parse(Console.ReadLine());
+                            switch (opcao)
+                            {
+                                case 1:
+                                    func.Andar();
+                                    break;
+                                case 2:
+                                    func.Comer();
+                                    break;
+                                case 3:
+                                    func.Trabalhar();
+                                    break;
+                                case 0:
+                                    Console.WriteLine("Até mais");
+                                    break;
+                                default:
+                                    Console.WriteLine("Opção inválida");
+                                    break;
+                            }
+                        } while (acao != 0);
+                        break;
+
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Obrigado por utilizar nosso sistema");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção inválida !");
+                        break;
+                }
             } while (opcao != 0);
         }
     }
--- ExemploDeHeranca/models/FuncionarioModel.cs
+++ ExemploDeHeranca/models/FuncionarioModel.cs
@@ -4,10 +4,23 @@ namespace ExemploDeHeranca.models
 {
     public class FuncionarioModel : PessoaModel
     {
-        public string Cargo{get; set;}
-        
-        public void Trabalhar(){
+        public string Cargo { get; set; }
+
+        public void Trabalhar()
+        {
             Console.WriteLine("Agora vamos trabalhar");
+        }
+
+        public void CadastrarFuncionario()
+        {
+            Console.WriteLine("Digite o nome do funcionário: ");
+            Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade: ");
+            Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o cargo do funcionário: ");
+            Cargo = Console.ReadLine();
         }
     }
 }
