6dd21ef3968567ebc2929f290aa272d823bd2fbb
Gabriel Moraes <gmmartins06@gmail.com>
2019-10-01 14:37:51 -0300

    Sistema completo

--- Cadastro MVC/Controller/UsuarioController.cs
+++ Cadastro MVC/Controller/UsuarioController.cs
@@ -1,11 +1,64 @@
 using System;
+using System.Collections.Generic;
+using Cadastro_MVC.models;
 
 namespace Cadastro_MVC.Controller
 {
     public class UsuarioController
     {
-        public void CadastroUsuario(){
+        List<UsuarioModel> listaDeUsuario = new List<UsuarioModel>();
+        public void CadastroUsuario()
+        {
+
             Console.WriteLine("Vamos cadastrar");
+            Console.WriteLine("Informe o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Digite a senha: ");
+            string senha = Console.ReadLine();
+
+            // Instanciando um novo objeto
+            UsuarioModel usuario = new UsuarioModel();
+
+            // Pegando o que foi escrito e armazenando no objeto 
+            usuario.Id = listaDeUsuario.Count + 1;
+            usuario.Nome = nome;
+            usuario.Email = email;
+            usuario.Senha = senha;
+            usuario.DataCriacao = DateTime.Now;
+            listaDeUsuario.Add(usuario);
+            Console.WriteLine("Obrigado por cadastrar usuario");
+        }
+
+        public void ListarUsuario()
+        {
+            Console.Clear();
+            foreach (var item in listaDeUsuario)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine($"Usuário => {item.Nome}\nId => {item.Id}\nEmail => {item.Email}\nData de criação => {item.DataCriacao}");
+                Console.WriteLine("-----------------------");
+            }
+        }
+
+        public bool Logar()
+        {
+            Console.WriteLine("Digite o e-mail: ");
+            string email = Console.ReadLine();
+            Console.WriteLine("Digite a senha");
+            string senha = Console.ReadLine();
+
+            foreach (var item in listaDeUsuario)
+            {
+                if (item.Email == email && item.Senha == senha)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
--- Cadastro MVC/Program.cs
+++ Cadastro MVC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Cadastro_MVC.Controller;
 
 namespace Cadastro_MVC
 {
@@ -6,7 +7,43 @@ namespace Cadastro_MVC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UsuarioController usuarioController = new UsuarioController();
+            int opcao = 0;
+            do
+            {
+                Console.WriteLine("Escolha uma opção");
+                Console.WriteLine("1 - Cadastrar Usuário");
+                Console.WriteLine("2 - Listar Usuários");
+                Console.WriteLine("3 - Autenticação");
+                Console.WriteLine("0 - Sair");
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        usuarioController.CadastroUsuario();
+                        break;
+                    case 2:
+                        usuarioController.ListarUsuario();
+                        break;
+                    case 3:
+                        bool usuarioRetornado = usuarioController.Logar();
+                        if (usuarioRetornado)
+                        {
+                            Console.WriteLine("Usuário localizado");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Usuário não encontrado");
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("Obrigado por usar nosso sistema");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            } while (opcao != 0);
         }
     }
 }
